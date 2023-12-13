5918498bbb600eb6a1aaf62994ebef0e8b2f1944
tcandra24 <tcandra007@gmail.com>
2023-12-13 16:19:59 +0700

    Rev. Sidebar

--- resources/views/sections/sidebar.blade.php
+++ resources/views/sections/sidebar.blade.php
@@ -15,8 +15,8 @@
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">Home</span>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/dashboard" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->is('/dashboard')) ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->is('/dashboard')) ? 'active' : '' }}" href="/dashboard" aria-expanded="false">
                         <span>
                             <i class="ti ti-layout-dashboard"></i>
                         </span>
@@ -27,8 +27,8 @@
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">Transaksi</span>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/transactions/izin-keluar" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->segment(1) . '/' . request()->segment(2) == 'transactions/izin-keluar') ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->segment(1) . '/' . request()->segment(2) == 'transactions/izin-keluar') ? 'active' : '' }}" href="/transactions/izin-keluar" aria-expanded="false">
                         <span>
                             <i class="ti ti-database"></i>
                         </span>
@@ -39,16 +39,16 @@
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">Approval</span>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/izin-keluar" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->segment(1) == 'izin-keluar') ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->segment(1) == 'izin-keluar') ? 'active' : '' }}" href="/izin-keluar" aria-expanded="false">
                         <span>
                             <i class="ti ti-lock"></i>
                         </span>
                         <span class="hide-menu">Izin Keluar</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/log-approval" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->is('/log-approval')) ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->is('/log-approval')) ? 'active' : '' }}" href="/log-approval" aria-expanded="false">
                         <span>
                             <i class="ti ti-notes"></i>
                         </span>
@@ -60,24 +60,24 @@
                     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
                     <span class="hide-menu">Pengaturan</span>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/pengguna" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->is('/pengguna')) ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->is('/pengguna')) ? 'active' : '' }}" href="/pengguna" aria-expanded="false">
                         <span>
                             <i class="ti ti-user"></i>
                         </span>
                         <span class="hide-menu">Pengguna</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/permissions" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->is('/permissions')) ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->is('/permissions')) ? 'active' : '' }}" href="/permissions" aria-expanded="false">
                         <span>
                             <i class="ti ti-door"></i>
                         </span>
                         <span class="hide-menu">Ijin</span>
                     </a>
                 </li>
-                <li class="sidebar-item">
-                    <a class="sidebar-link" href="/roles" aria-expanded="false">
+                <li class="sidebar-item {{ (request()->is('/roles')) ? 'selected' : '' }}">
+                    <a class="sidebar-link {{ (request()->is('/roles')) ? 'active' : '' }}" href="/roles" aria-expanded="false">
                         <span>
                             <i class="ti ti-settings"></i>
                         </span>
